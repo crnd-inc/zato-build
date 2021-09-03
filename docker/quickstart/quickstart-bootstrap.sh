ddf00382b8428b156330395919dee635cbbea117
Dariusz Suchojad <dsuch-github@m.zato.io>
2021-09-03 21:16:51 +0200

    Redis connections are now optional.

--- docker/quickstart/quickstart-bootstrap.sh
+++ docker/quickstart/quickstart-bootstrap.sh
@@ -3,7 +3,7 @@ source /etc/environment
 
 cd /opt/zato/env/qs-1 || exit 1
 set -x # enable show commands
-$ZATO_BIN quickstart create ${ODB_DATA} --kvdb_password '' --servers "${SERVERS_COUNT:-1}" /opt/zato/env/qs-1/ "${ODB_TYPE:-postgresql}" "${REDIS_HOSTNAME:-localhost}" 6379
+$ZATO_BIN quickstart create ${ODB_DATA} --kvdb_password '' --servers "${SERVERS_COUNT:-1}" /opt/zato/env/qs-1/ --odb_type "${ODB_TYPE:-postgresql}" --kvdb_host "${REDIS_HOSTNAME:-localhost}" --kvdb_port 6379
 $ZATO_BIN set-ide-password --verbose --password "${ZATO_IDE_PUBLISHER_PASSWORD}" /opt/zato/env/qs-1/server1 |tee /opt/zato/zato_ide_password
 [[ -d /opt/zato/env/qs-1/web-admin ]] && sed -i -e 's/dashboard/web-admin/' /opt/zato/update_password.config
 $ZATO_BIN from-config /opt/zato/update_password.config
